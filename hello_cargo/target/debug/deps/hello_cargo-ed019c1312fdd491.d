2db702ddf3e1733a98c42cbbd1ee5eb4bee460fd
Ian Price <ianprice943@yahoo.com>
2020-05-19 01:20:19 -0400

    Hello, Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-ed019c1312fdd491.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/hello_cargo/target/debug/deps/hello_cargo-ed019c1312fdd491: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/hello_cargo/target/debug/deps/hello_cargo-ed019c1312fdd491.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ianprice943/projects/rust/rustBook/hello_cargo/target/debug/hello_cargo: /home/ianprice943/projects/rust/rustBook/hello_cargo/src/main.rs
