9bab8791919821f7a636d268f061b20d41b2a5e4
Ian Price <ianprice943@yahoo.com>
2020-05-22 23:17:41 -0400

    fixing placement of code

---
+++ guessing_game/target/rls/debug/deps/guessing_game-556b32fb6518359f.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/guessing_game/target/rls/debug/deps/guessing_game-556b32fb6518359f.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/guessing_game/target/rls/debug/deps/guessing_game-556b32fb6518359f.d: src/main.rs
+
+src/main.rs:
