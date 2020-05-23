a5a923e5f06f1333ab43a315608f8012628f553c
Ian Price <ianprice943@yahoo.com>
2020-05-23 00:04:45 -0400

    remove target folder

--- guessing_game/target/rls/debug/build/libc-1084d1e3be2e5d41/build_script_build-1084d1e3be2e5d41.d
+++
@@ -1,5 +0,0 @@
-/home/ianprice943/projects/rust/rustBook/guessing_game/target/rls/debug/build/libc-1084d1e3be2e5d41/build_script_build-1084d1e3be2e5d41: /home/ianprice943/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.70/build.rs
-
-/home/ianprice943/projects/rust/rustBook/guessing_game/target/rls/debug/build/libc-1084d1e3be2e5d41/build_script_build-1084d1e3be2e5d41.d: /home/ianprice943/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.70/build.rs
-
-/home/ianprice943/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.70/build.rs:
--- guessing_game/target/rls/debug/deps/guessing_game-316dc34d12ccc199.d
+++
@@ -1,5 +0,0 @@
-/home/ianprice943/projects/rust/rustBook/guessing_game/target/rls/debug/deps/guessing_game-316dc34d12ccc199.rmeta: src/main.rs
-
-/home/ianprice943/projects/rust/rustBook/guessing_game/target/rls/debug/deps/guessing_game-316dc34d12ccc199.d: src/main.rs
-
-src/main.rs:
