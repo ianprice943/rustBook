0e5695b3d2c0048b268ac7ba01faaa7b2ec7624c
Ian Price <ianprice943@yahoo.com>
2020-05-23 00:00:36 -0400

    remove target?

--- hello_cargo/target/debug/deps/hello_cargo-ff97f97528de4372.d
+++
@@ -1,5 +0,0 @@
-/home/ianprice943/projects/rust/rustBook/hello_cargo/target/debug/deps/hello_cargo-ff97f97528de4372.rmeta: src/main.rs
-
-/home/ianprice943/projects/rust/rustBook/hello_cargo/target/debug/deps/hello_cargo-ff97f97528de4372.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/ianprice943/projects/rust/rustBook/hello_cargo/target/debug/hello_cargo: /home/ianprice943/projects/rust/rustBook/hello_cargo/src/main.rs
