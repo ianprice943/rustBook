923c28ce3b7cbab50ebb67f8cb24306ec9299e1c
Ian Price <ianprice943@yahoo.com>
2020-08-12 00:51:33 -0400

    copy

--- chapter4/c4_6_stack_only_data_copy/src/main.rs
+++ chapter4/c4_6_stack_only_data_copy/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y); //this is valid because integers are easy to copy on
+                                      //the stack and have a known memory size at compile time
 }
