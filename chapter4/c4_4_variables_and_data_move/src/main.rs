dd38068d64c9aee5ad4b5e8dabb25d9243469453
Ian Price <ianprice943@yahoo.com>
2020-08-12 00:37:13 -0400

    data move

--- chapter4/c4_4_variables_and_data_move/src/main.rs
+++ chapter4/c4_4_variables_and_data_move/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = x; //Two variables representing 5 have been pushed on the stack.
+               //Stack is used because integer types have a fixed memory size.
+    
+    let s1 = String::from("hello");
+    let s2 = s1; //pointer for s1 and s2 point at the same location in the heap
+                 //s1 is also considered invalid now to avoid "double free" memory corruption
 }
---
+++ chapter4/c4_4_variables_and_data_move/target/rls/debug/deps/c4_4_variables_and_data_move-cf0ced7b2b556bda.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_4_variables_and_data_move/target/rls/debug/deps/c4_4_variables_and_data_move-cf0ced7b2b556bda.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_4_variables_and_data_move/target/rls/debug/deps/c4_4_variables_and_data_move-cf0ced7b2b556bda.d: src/main.rs
+
+src/main.rs:
