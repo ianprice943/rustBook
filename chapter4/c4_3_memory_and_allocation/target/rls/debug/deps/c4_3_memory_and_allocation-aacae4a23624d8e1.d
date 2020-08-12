cf07d5619cdeb95548200fc0680c9cb093bad656
Ian Price <ianprice943@yahoo.com>
2020-08-11 20:50:25 -0400

    fin

---
+++ chapter4/c4_3_memory_and_allocation/target/rls/debug/deps/c4_3_memory_and_allocation-aacae4a23624d8e1.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_3_memory_and_allocation/target/rls/debug/deps/c4_3_memory_and_allocation-aacae4a23624d8e1.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_3_memory_and_allocation/target/rls/debug/deps/c4_3_memory_and_allocation-aacae4a23624d8e1.d: src/main.rs
+
+src/main.rs:
---
+++ chapter4/c4_3_memory_and_allocation/target/rls/debug/deps/c4_3_memory_and_allocation-f9b02dc4f3bbddc2.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_3_memory_and_allocation/target/rls/debug/deps/c4_3_memory_and_allocation-f9b02dc4f3bbddc2.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_3_memory_and_allocation/target/rls/debug/deps/c4_3_memory_and_allocation-f9b02dc4f3bbddc2.d: src/main.rs
+
+src/main.rs:
