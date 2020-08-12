ca28d6ff125391deeb25919868a5aa52e246bd49
Ian Price <ianprice943@yahoo.com>
2020-08-11 20:30:40 -0400

    scope

---
+++ chapter4/c_4_8_return_values_and_scope/target/rls/debug/deps/c_4_8_return_values_and_scope-21b64a4c1139355b.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/chapter4/c_4_8_return_values_and_scope/target/rls/debug/deps/c_4_8_return_values_and_scope-21b64a4c1139355b.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/chapter4/c_4_8_return_values_and_scope/target/rls/debug/deps/c_4_8_return_values_and_scope-21b64a4c1139355b.d: src/main.rs
+
+src/main.rs:
---
+++ chapter4/c_4_8_return_values_and_scope/target/rls/debug/deps/c_4_8_return_values_and_scope-9b532a1335c5f50b.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/chapter4/c_4_8_return_values_and_scope/target/rls/debug/deps/c_4_8_return_values_and_scope-9b532a1335c5f50b.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/chapter4/c_4_8_return_values_and_scope/target/rls/debug/deps/c_4_8_return_values_and_scope-9b532a1335c5f50b.d: src/main.rs
+
+src/main.rs:
