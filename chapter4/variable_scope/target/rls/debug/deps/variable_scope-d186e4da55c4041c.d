159cd4d9c73b9a1d52f0d988305b22aee4c98de4
Ian Price <ianprice943@yahoo.com>
2020-08-11 20:24:04 -0400

    Renaming

--- chapter4/variable_scope/target/rls/debug/deps/variable_scope-d186e4da55c4041c.d
+++
@@ -1,5 +0,0 @@
-/home/ianprice943/projects/rust/rustBook/chapter4/variable_scope/target/rls/debug/deps/variable_scope-d186e4da55c4041c.rmeta: src/main.rs
-
-/home/ianprice943/projects/rust/rustBook/chapter4/variable_scope/target/rls/debug/deps/variable_scope-d186e4da55c4041c.d: src/main.rs
-
-src/main.rs:
