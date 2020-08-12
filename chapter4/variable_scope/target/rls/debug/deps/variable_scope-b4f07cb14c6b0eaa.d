129f4387d904a94b8fd61435e735867213691e41
Ian Price <ianprice943@yahoo.com>
2020-08-11 20:18:24 -0400

    Init chapter 4

---
+++ chapter4/variable_scope/target/rls/debug/deps/variable_scope-b4f07cb14c6b0eaa.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/chapter4/variable_scope/target/rls/debug/deps/variable_scope-b4f07cb14c6b0eaa.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/chapter4/variable_scope/target/rls/debug/deps/variable_scope-b4f07cb14c6b0eaa.d: src/main.rs
+
+src/main.rs:
