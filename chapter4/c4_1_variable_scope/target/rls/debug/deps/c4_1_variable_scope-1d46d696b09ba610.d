08238541460ec6cae9bc37934a342da3e844c7df
Ian Price <ianprice943@yahoo.com>
2020-08-11 20:32:35 -0400

    variable scope

---
+++ chapter4/c4_1_variable_scope/target/rls/debug/deps/c4_1_variable_scope-1d46d696b09ba610.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_1_variable_scope/target/rls/debug/deps/c4_1_variable_scope-1d46d696b09ba610.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_1_variable_scope/target/rls/debug/deps/c4_1_variable_scope-1d46d696b09ba610.d: src/main.rs
+
+src/main.rs:
---
+++ chapter4/c4_1_variable_scope/target/rls/debug/deps/c4_1_variable_scope-8ec8e40a784bc27e.d
@@ -0,0 +1,5 @@
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_1_variable_scope/target/rls/debug/deps/c4_1_variable_scope-8ec8e40a784bc27e.rmeta: src/main.rs
+
+/home/ianprice943/projects/rust/rustBook/chapter4/c4_1_variable_scope/target/rls/debug/deps/c4_1_variable_scope-8ec8e40a784bc27e.d: src/main.rs
+
+src/main.rs:
