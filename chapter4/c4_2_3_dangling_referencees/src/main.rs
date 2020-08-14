49e04bf0c656c25b6453966d966d266a448d8bb7
Ian Price <ianprice943@yahoo.com>
2020-08-14 00:29:18 -0400

    dangling references

--- chapter4/c4_2_3_dangling_referencees/src/main.rs
+++ chapter4/c4_2_3_dangling_referencees/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let reference_to_nothing = dangle();
+}
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s //at this point s has been freed, but we're trying to return a reference to it
+    //the solution would be to just return s itself like below
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
 }
