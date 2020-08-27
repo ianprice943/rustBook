66d0062eec02d97373c5d14422d7cd7a1d6c77c5
Ian Price <ianprice943@yahoo.com>
2020-08-26 23:45:07 -0400

    chapter 5 complete

--- chapter5/c5_3_method_syntax/src/main.rs
+++ chapter5/c5_3_method_syntax/src/main.rs
@@ -8,6 +8,23 @@ impl Rectangle {
     fn area(&self) -> u32 {         // rust knows that this is of type Rectangle, because it is in the
         self.width * self.height    // implementation block of Rectangle
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+}
+
+impl Rectangle {    // we are allowed to have multiple impl blocks for a struct
+    fn perimeter(&self) -> u32 {
+        2 * (self.width + self.height)
+    }
 }
 
 fn main() {
@@ -15,9 +32,22 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let sq = Rectangle::square(3);
 
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
-}
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("The area of the square is {} square pixels", sq.area());
+    println!("The perimeter of rect2 is {} pixels", rect2.perimeter());
+} 
