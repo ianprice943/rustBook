37efb93b2c035b56194cce296fbcbdf8b66ab19a
Ian Price <ianprice943@yahoo.com>
2020-09-01 00:29:06 -0400

    chapter 6 complete

--- chapter6/c6_3_concise_control_flow_with_if_let/src/main.rs
+++ chapter6/c6_3_concise_control_flow_with_if_let/src/main.rs
@@ -1,3 +1,41 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),                        // a lot of boiler plate just to check for 3
+    }
+
+    if let Some(3) = some_u8_value {    // behaves the same way as above, but with less code.
+        println!("three");              // this does however remove the exhaustive checking of
+    }                                   // match.
+
+    // these two give the same result, it's up to the programmer to decide which is more appropriate
+    // for their use case
+    let coin = Coin::Penny;
+    let mut count = 0;
+    match &coin {   // & added to allow borrowing instead of move
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
 }
