cc06aa3086d1b2f2e9f708acfc6a8e5b844a8a23
Ian Price <ianprice943@yahoo.com>
2020-09-01 00:22:10 -0400

    the match control flow operator

--- chapter6/c6_2_the_match_control_flow_operator/src/main.rs
+++ chapter6/c6_2_the_match_control_flow_operator/src/main.rs
@@ -1,3 +1,81 @@
+/*
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+    //can use curly brackets to run more than one line of code,
+    //but last line is what will be returned
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+*/
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+// Matching wit hOption<T>
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,           // if this were not here, we would get a compiler error
+        Some(i) => Some(i + 1),
+    }
+}
+
+    
 fn main() {
-    println!("Hello, world!");
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = 0u8;    // the _ placeholder allows us to only handle the "arms"
+    match some_u8_value {       // we want/need to, and it tells Rust we can ignore the rest
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
 }
