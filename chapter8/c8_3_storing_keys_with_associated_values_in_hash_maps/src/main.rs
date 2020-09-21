ae626e86785bfaa8efa48dda6036046db9d60f64
Ian Price <ianprice943@yahoo.com>
2020-09-20 20:17:28 -0400

    chapter 8 complete

--- chapter8/c8_3_storing_keys_with_associated_values_in_hash_maps/src/main.rs
+++ chapter8/c8_3_storing_keys_with_associated_values_in_hash_maps/src/main.rs
@@ -1,3 +1,81 @@
 fn main() {
-    println!("Hello, world!");
+    use std::collections::HashMap;
+
+    // basic hashmap construction, each key must be of the same type,
+    // and each value must also be of the same type (key and value
+    // can be different types however)
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // convert vec of tuples into hashmap
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let mut scores: HashMap<_, _> =
+        teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+    // hash maps and ownership
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+    // println!("{}", field_name); // borrow of moved value error in this case
+
+    // accessing values in a hash map
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name); // value will be Some(&10), and if "Blue" had no
+                                        // value attached to it, then we would get None.
+                                        // This means we need handle Option<&V> cases.
+
+    // Iterating over each key/value pair
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    // updating a hash map
+    // method 1, overwriting values
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores);
+
+    // only inserting a value if the key has no value
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    // updating a value based on the old value
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
