146bc910ba2206735c57b71f2bfabde638d3b08d
Ian Price <ianprice943@yahoo.com>
2020-10-22 16:47:42 -0400

    partway through traits

--- chapter10/c10_2_traits_defining_shared_behavior/src/lib.rs
+++ chapter10/c10_2_traits_defining_shared_behavior/src/lib.rs
@@ -1,5 +1,9 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
 }
 
 pub struct NewsArticle {
@@ -10,8 +14,11 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
+    /*fn summarize(&self) -> String {   // leaving this commented out will force default behavior
         format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }*/
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
     }
 }
 
@@ -25,5 +32,67 @@ pub struct Tweet {
 impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// traits as parameters
+
+// this function doesn't ask for a type on the item, it instead asks for any parameter which
+// implements the Summary trait
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// trait bound syntax
+
+// impl Trait works for straightforward cases, but is syntactic sugar for a longer form known as a "trait bound"
+
+pub fn notify_tbound<T: Summary>(item: &T) {
+    println!("Breaking news!{}", item.summarize());
+}
+
+// this works if we don't care what the types of item1 and item2 are
+pub fn notify_extra_params(item1: &impl Summary, item2: &impl Summary) {
+    println!("Double breaking news!{}, {}", item1.summarize(), item2.summarize());
+}
+
+// if we do want to ensure they're both the same type, we can do this:
+pub fn notify_extra_params_tbound<T: Summary>(item1: &T, item2: &T) {
+    println!("Double breaking news, this time with same types!{}, {}", item1.summarize(), item2.summarize());
+}
+
+// specifying multiple trait bounds with the + syntax
+
+// Display isn't defined in this scope, so this will no longer compile
+pub fn notify_plus(item: &(impl Summary + Display)) {}
+
+// also valid for trait bounds on generic types
+pub fn notify_plus_tbound<T: Summary + Display>(item: &T) {}
+
+// clearer trait bounds with where clauses
+
+// instead of writing this
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {}
+
+// we can do 
+fn some_function_where<T, U>(t: &T, u: &U) -> i32
+    where T: Display + Clone,
+          U: Clone + Debug
+{}
+
+// returning types that implement traits
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
     }
 }
