10ef064c41da061da13eef84e1870e947a814926
Ian Price <ianprice943@yahoo.com>
2021-05-06 01:11:37 -0400

    almost finished with module

---
+++ chapter11/c11_1_how_to_write_tests/adder/src/lib.rs
@@ -0,0 +1,113 @@
+/*#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+}*/
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// checking results with the assert! Macro
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+        //self.width < other.width && self.height > other.height
+    }
+}
+
+//Testing equality with the assert_eq! and assert_ne! Macros
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+    //a + 3
+}
+
+//Adding custom failure messages
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+    //String::from("Hello!")
+}
+
+//Checking for Panics with should_panic
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // checking results with the assert! Macro
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    //Testing equality with the assert_eq! and assert_ne! Macros   
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    //Adding custom failure messages
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was '{}'",
+            result
+        );
+    }
+
+    //Checking for Panics with should_panic
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
+fn main() {}
