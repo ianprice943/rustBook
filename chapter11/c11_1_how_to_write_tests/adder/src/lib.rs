358bf018b8718c142261958917f8093f8dfada4c
Ian Price <ianprice943@yahoo.com>
2021-05-27 11:06:17 -0400

    finished 11.1

--- chapter11/c11_1_how_to_write_tests/adder/src/lib.rs
+++ chapter11/c11_1_how_to_write_tests/adder/src/lib.rs
@@ -42,10 +42,39 @@ pub struct Guess {
     value: i32,
 }
 
+/*impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 /*|| value > 100*/ {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}*/
+// more precise should_panic behaviour
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        /*if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }*/
+        if value < 1 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
         }
 
         Guess { value }
@@ -104,9 +133,18 @@ mod tests {
 
     //Checking for Panics with should_panic
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
 
