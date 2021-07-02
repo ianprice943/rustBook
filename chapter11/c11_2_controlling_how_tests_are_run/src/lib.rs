471f6f0da4fe64d60eadb2616a48fe2b25d9f1b1
ianprice943@yahoo.com <ianprice943@yahoo.com>
2021-07-02 18:27:57 -0400

    Chapter 11.2 complete

---
+++ chapter11/c11_2_controlling_how_tests_are_run/src/lib.rs
@@ -0,0 +1,62 @@
+// Showing Function Out put with `cargo test -- -- show-output
+
+// fn prints_and_returns_10(a: i32) -> i32 {
+//     println!("I got the value {}", a);
+//     10
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn this_test_will_pass() {
+//         let value = prints_and_returns_10(4);
+//         assert_eq!(10, value);
+//     }
+
+//     #[test]
+//     fn this_test_will_fail() {
+//         let value = prints_and_returns_10(8);
+//         assert_eq!(5, value);
+//     }
+// }
+
+// Running a Subset of Tests by Name with `cargo test one_hundred or Multiple Tests with `cargo test add`
+
+// pub fn add_two(a: i32) -> i32 {
+//     a + 2
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn add_two_and_two() {
+//         assert_eq!(4, add_two(2));
+//     }
+
+//     #[test]
+//     fn add_three_and_two() {
+//         assert_eq!(5, add_two(3));
+//     }
+
+//     #[test]
+//     fn one_hundred() {
+//         assert_eq!(102, add_two(100));
+//     }
+// }
+
+// Ignoring Some Tests Unless Specifically Requested
+
+#[test]
+fn it_works() {
+    assert_eq!(2 + 2, 4);
+}
+
+#[test]
+#[ignore]
+fn expensive_test() {
+    // code that takes an hour to run
+}
